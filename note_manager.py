c6ed4bdcc7d2d6f3ae336c52410d1a4d61c8a985
Utkarsh Pratik <btech10711.22@bitmesra.ac.in>
2025-07-01 03:11:33 +0530

    Update note_manager.py

--- note_manager.py
+++ note_manager.py
@@ -2,12 +2,12 @@
 from encryption import encrypt, decrypt
 from datetime import datetime
 from bson import ObjectId
-from db_info import notes_col
+from db.mongo import get_notes_collection
 import streamlit as st
 
 @st.cache_data(ttl=600) # Cache for 10 minutes
 def load_notes(user_id):
-    notes = list(notes_col.find({"user_id": ObjectId(user_id)}))
+    notes = list(get_notes_collection.find({"user_id": ObjectId(user_id)}))
     for note in notes:
         note["id"] = str(note["_id"])
         note["content"] = decrypt(note["content"])
@@ -20,9 +20,9 @@ def save_notes(user_id, notes):
             note["content"] = encrypt(note["content"])
             if "_id" in note:
                 note["_id"] = ObjectId(note["_id"])
-                notes_col.replace_one({"_id": note["_id"]}, note)
+                get_notes_collection.replace_one({"_id": note["_id"]}, note)
             else:
-                notes_col.insert_one(note)
+                get_notes_collection.insert_one(note)
         return True
     except Exception as e:
         print("Error saving notes:", e)
@@ -39,19 +39,19 @@ def add_note(user_id, title, content, tags, subject, folder=None, favorite=False
         "favorite": favorite,
         "created_at": datetime.now().isoformat()
     }
-    result = notes_col.insert_one(note)
+    result = get_notes_collection.insert_one(note)
     note["id"] = str(result.inserted_id)
     note["content"] = content  # decrypted for in-app use
     return note
 
 def update_notes_after_folder_rename(user_id, old, new):
-    notes_col.update_many(
+    get_notes_collection.update_many(
         {"user_id": ObjectId(user_id), "folder": old},
         {"$set": {"folder": new}}
     )
 
 def update_notes_after_folder_delete(user_id, folder_name):
-    notes_col.update_many(
+    get_notes_collection.update_many(
         {"user_id": ObjectId(user_id), "folder": folder_name},
         {"$unset": {"folder": ""}}
     )
